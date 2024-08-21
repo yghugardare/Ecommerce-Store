e433ba1d6c6a40d8bb9fb6cef950a0be0597e345
Yash Ghugardare <yghugardare6@gmail.com>
2024-08-21 22:10:40 +0530

    admin skeleton

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function page() {
   return (
-    <div>Admin Page</div>
+    <div>
+      <div>
+        Admin Page
+      </div>
+      <div>
+        Some routes
+      </div>
+      <div>Orders</div>
+    </div>
   )
 }
 
